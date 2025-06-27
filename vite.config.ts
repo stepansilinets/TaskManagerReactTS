235b4c9de8959514b34827943a528ccbe28269d3
whoami <styopaavangard@Styopa-Avangard.local>
2025-06-28 00:32:31 +0700

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite'; // Используем Vite-плагин вместо PostCSS
+
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-});
+  base: '/TaskManagerReactTS /', // Замени на имя своего репозитория
+  plugins: [react()]
+})
